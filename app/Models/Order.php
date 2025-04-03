62c1cea943afeaffec8236027a83e12faf7d61ca
AKubryak <96373690+AKubryak@users.noreply.github.com>
2025-04-03 21:10:44 +0300

    Add models with relationships

---
+++ app/Models/Order.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'customer_name',
+        'phone',
+        'address',
+        'comment',
+        'total',
+        'status'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class, 'order_items')
+            ->withPivot('quantity', 'price')
+            ->withTimestamps();
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
